ded41960510e42b698e9b19bfe2f12563eb3f050
L-O-S-S <62038551+L-O-S-S@users.noreply.github.com>
2021-04-15 17:01:44 -0300

    -

---
+++ Data/IESDbInitializer.cs
@@ -0,0 +1,45 @@
+﻿using ExercicioAluno.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExercicioAluno.Data
+{
+    public class IESDbInitializer
+    {
+        public static void Initialize(IESContext context)
+        {
+            context.Database.EnsureCreated();
+
+            if (context.Alunos.Any())
+            {
+                return;
+            }
+
+            var alunos = new Aluno[]
+            {
+                new Aluno{Nome = "Ana", Idade=12, Genero="F", Sala="01", Turma="1A"},
+                new Aluno{Nome = "Beatriz", Idade=12, Genero="F", Sala="01", Turma="1A"},
+                new Aluno{Nome = "Carlos", Idade=13, Genero="M", Sala="06", Turma="2C"},
+                new Aluno{Nome = "Daniel", Idade=11, Genero="M", Sala="02", Turma="1B"},
+                new Aluno{Nome = "Elias", Idade=13, Genero="M", Sala="05", Turma="2B"},
+                new Aluno{Nome = "Fernanda", Idade=14, Genero="F", Sala="07", Turma="3A"},
+                new Aluno{Nome = "Gabriel", Idade=11, Genero="M", Sala="01", Turma="1A"},
+                new Aluno{Nome = "Higor", Idade=12, Genero="M", Sala="02", Turma="1B"},
+                new Aluno{Nome = "Rubens", Idade=13, Genero="M", Sala="04", Turma="2A"},
+                new Aluno{Nome = "Joana", Idade=11, Genero="F", Sala="03", Turma="1C"},
+
+            };
+
+
+            foreach (Aluno a in alunos)
+            {
+                context.Alunos.Add(a);
+            }
+
+            context.SaveChanges();
+
+        }
+    }
+}
---
+++ Models/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExercicioAluno.Models
+{
+    public class Aluno
+    {
+        [Key]
+        public long? AlunoID { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+
+        public int Idade { get; set; }
+        [Required]
+        public string Genero { get; set; }
+
+        [Required]
+        public string Sala { get; set; }
+        [Required]
+        public string Turma { get; set; }
+    }
+}
